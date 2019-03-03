39b630cd43ad57292d19aa982e2e715f023b3d20
IanL126 <ialoam7376@gmail.com>
2019-03-03 12:55:14 -0800

    3/3/19

--- src/extra/FruitQuiz.java
+++ src/extra/FruitQuiz.java
@@ -1,7 +1,7 @@
 package extra;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
+//    Level 0
 
 import java.applet.AudioClip;
 import java.awt.event.KeyAdapter;
@@ -14,19 +14,29 @@ import javax.swing.JLabel;
 public class FruitQuiz extends KeyAdapter {
 
 	void makeQuestions() {
-		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
-		// 11. Make another question called "question2".  Use question1 above as a guide.
+		question1 = new JLabel(
+				"<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+		// 11. Make another question called "question2". Use question1 above as a guide.
 
-	question2 = new JLabel("<html>Which of these animals is native to Hawaii? <br> A: Veiled Chameleon <br> B: Cane Toad</html> C: Apple Snail <br>");
-	}@Override
+		question2 = new JLabel(
+				"<html>Which of these animals is native to Hawaii? <br> A: Nene <br> B: Cane Toad <br> C: Apple Snail </html>");
+
+		 question3 = new JLabel("<html>Who was the tenth president of the US<br> A: James Polk<br> B: John Tyler <br> C: William Henry Harrison </html>");
+
+		question4 = new JLabel("<html>Which of the following wars was America's bloodiest conflict? <br> A: Civil War<br> B: World Ward 2 <br> C: World Ward 1 </html>");
+	}
+
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+int a = 65;
+int b = 66;
+int c = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
-		
+
+	
 		// 12. If question2 is showing,
 			
 			// 13. check if it is right or wrong like you did for question1
@@ -36,30 +46,63 @@ public class FruitQuiz extends KeyAdapter {
 			// 3. If they selected the right fruit, do steps 4 and 7
 			
 				// 4. Call the correct() method
-				
+			if (keyCode==c) {
+				correct();
+				nextQuestion(question2);
+			}
 				// 7. Use the nextQuestion() method to go to question2
 			
 			
 			// 8. else (if they touched something else)
-				
+			else if(keyCode==a || keyCode==b) {
+				incorrect();
+			}
+			
 				// 9. Call the incorrect() method
 		
 		}
-
+if(question2.isShowing()) {
+	if (keyCode==a) {
+		correct();
+		nextQuestion(question3);
+	}
+	else if (keyCode== b || keyCode== c) {
+		incorrect();
+	}
+}
+if(question3.isShowing()) {
+	if (keyCode==b) {
+		correct();
+		nextQuestion(question4);
+	}
+	else if (keyCode== a || keyCode== c) {
+		incorrect();
+	}
+}
+if(question4.isShowing()) {
+	if (keyCode==a) {
+		correct();
+System.exit(0);
+	}
+	else if (keyCode== b || keyCode== c) {
+		incorrect();
+	}
+}
 	}
 
 	private void correct() {
 		// 5. Find a sound for when they get a question right, and drag it into
-		// the 'extra' package. It must be a .wav file. 
+		// the 'extra' package. It must be a .wav file.
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+		playSound("correct.wav");
 
 	}
 
 	private void incorrect() {
-		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
-
+		// 10. Find a sound for wrong answers and put it in the default package. Use the
+		// playSound method to play it.
+playSound("wrong.wav");
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
@@ -95,4 +138,3 @@ public class FruitQuiz extends KeyAdapter {
 	JLabel question1 = new JLabel(), question2 = new JLabel(), question3 = new JLabel(), question4 = new JLabel(),
 			question5;
 }
-
--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 public class SimpleCalculator {
 	public static void main(String[] args) {
 		
-	
+		for (int i = 0; i < 1000; i++) {
 		// 1. Get 2 numbers from the user and convert them to integer.
 String number1 = JOptionPane.showInputDialog(null, "Give me a number");
 String number2 = JOptionPane.showInputDialog(null, "Give me another number");
@@ -17,9 +17,31 @@ double num2 = Integer.parseInt(number2);
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Subtract", "Multiply", "Divide" },
 				null);
 double divide = num1/num2;
+double add=num1+num2;
+double subtract=num1-num2;
+double multiply=num1*num2;
 		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-if (operation==0) {
+if (operation==3) {
 	JOptionPane.showMessageDialog(null, number1 + "/"+ number2 + "="+ divide);
+}
+if (operation==0) {
+	JOptionPane.showMessageDialog(null, number1 + "+"+ number2 + "="+ add);
+}
+if (operation==1) {
+	JOptionPane.showMessageDialog(null, number1 + "-"+ number2 + "="+ subtract);
+}
+if (operation==2) {
+	JOptionPane.showMessageDialog(null, number1 + "*"+ number2 + "="+ multiply);
+}
+int Calculator = JOptionPane.showOptionDialog(null, "Do you want to make another calculation?", "Calculator", 0,
+		JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Yes", "No"},
+		null);
+if (Calculator==0) {
+	i=i+1;
+}
+if (Calculator==1) {
+	JOptionPane.showMessageDialog(null, "Have a nice day ;)");
+	System.exit(0);}
 }
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
@@ -27,6 +49,17 @@ if (operation==0) {
 	// 3. Create method for addition operation.
 	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
 	//    HINT: use 'static void add(int num1, num2) { ... }
-
-	// 4. Create similar methods for subtraction, multiplication and division.
+	static void add(int num1, int num2) {
+		int num3 = num1+num2;
+		System.out.println(num3);}
+		// 4. Create similar methods for subtraction, multiplication and division.
+	static void subtract(int num1, int num2) {
+		int num3 = num1-num2;
+		System.out.println(num3);}
+	static void multiply(int num1, int num2) {
+		int num3 = num1+num2;
+		System.out.println(num3);}
+	static void divide(int num1, int num2) {
+		int num3 = num1+num2;
+		System.out.println(num3);}
 }
